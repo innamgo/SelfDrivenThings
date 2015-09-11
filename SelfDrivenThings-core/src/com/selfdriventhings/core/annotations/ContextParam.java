30ebf0340405fc8f7a65dbf3efe7b7b819b98867
Hoon Kim <innamgo@naver.com>
2015-09-11 17:36:50 +0900

    1st

---
+++ SelfDrivenThings-core/src/com/selfdriventhings/core/annotations/ContextParam.java
@@ -0,0 +1,13 @@
+package com.selfdriventhings.core.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface ContextParam {
+	String Key() default "";
+	String Value() default "";
+}
